8289b3319535375b7f9c891bd713129494d21505
Karen <karen.yonea@hotmail.com>
2019-02-27 11:46:48 -0300

    kk

---
+++ teste.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int int main(int argc, char const *argv[]) {
+  std::string nome;
+  std::int idade;
+  std::cout << "Entrada:" << '\n';
+  std::cin >> nome;
+  std::cout << nome << '\n';
+  return 0;
+}
