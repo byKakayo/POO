836a1cccff42c3706125fd8e4e3f56a01c30101d
Karen <karen.yonea@hotmail.com>
2019-03-20 10:21:52 -0300

    Passagem por referencia e prototipo seq_collatz

--- seq_collatz.cpp
+++ seq_collatz.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
-int collatz(int n, vector<int>* v) {
-  v->push_back(n);
+int collatz(int n, vector<int> &v);
+
+int main() {
+  int n;
+  vector<int> v;
+  cin >> n;
+  n = collatz(n, v);
+  v.push_back(n);
+  for (int x : v){
+    cout << x << " ";
+  }
+  cout << "\n";
+  return 0;
+}
+
+int collatz(int n, vector<int> &v) {
+  v.push_back(n);
   if(n%2 == 0){
     n = n/2;
   }else{
@@ -13,17 +28,4 @@ int collatz(int n, vector<int>* v) {
     n = collatz(n, v);
   }
   return n;
-}
-
-int main() {
-  int n;
-  vector<int> v;
-  cin >> n;
-  n = collatz(n, &v);
-  v.push_back(n);
-  for (int x : v){
-    cout << x << " ";
-  }
-  cout << "\n";
-  return 0;
 }
