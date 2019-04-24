70984cf378ca77bcfd4116f9042d6c054367d281
Karen <karen.yonea@hotmail.com>
2019-04-24 11:18:51 -0300

    Proj2 n funciona

--- proj2/proj2_v2.cpp
+++ proj2/proj2_v2.cpp
@@ -2,20 +2,25 @@
 
 using namespace std;
 
+//Estrutura do quadrado
 struct square {
   string id;
-  vector<double> v1;
-  vector<double> v2;
+  vector<float> v1;
+  vector<float> v2; //Vértice do quadrado oposto ao vértice dado
 };
 
 void intersec(square s1, square s2, int flag);
 
 int main() {
+  //Criação de um vetor de quadrados
   vector<square> squares;
-  ifstream dat("squares.dat");
+
+  //Leitura dos dados do arquivo .dat
+  ifstream dat("squares2.dat");
   if (dat.is_open()) {
     string line, id;
-    double v1x, v1y, size;
+    float v1x, v1y, size;
+    //Enquanto existirem linhas no arquivo guarda um novo quadrado no vetor
     while (getline(dat, line)) {
       square sq;
       istringstream s(line);
@@ -29,6 +34,7 @@ int main() {
     }
     dat.close();
   }
+  //Percorre o vetor de quadrados e verifica a intersecção de um par de quadrados pela função intersec
   for(int i = 0; i < (int)squares.size(); i++){
     for(int n = i+1; n < (int)squares.size(); n++){
       intersec(squares[i], squares[n], 0);
@@ -38,23 +44,25 @@ int main() {
 }
 
 void intersec(square s1, square s2, int flag) {
-  double a;
+  float a;
+  a = 0.0;
+  //Condição de não intersecção
   if((s1.v2[0] <= s2.v1[0] || s2.v2[0] <= s1.v1[0]) || (s1.v2[1] <= s2.v1[1] || s2.v2[1] <= s1.v1[1])) {
     return;
   }else{
-    if(s1.v1[0] > s2.v1[0] && s1.v1[1] > s2.v1[1] && s2.v2[0] > s1.v2[0] && s2.v2[1] > s1.v2[1]){ //s1 dentro de s2
+    if(s1.v1[0] >= s2.v1[0] && s1.v1[1] >= s2.v1[1] && s2.v2[0] >= s1.v2[0] && s2.v2[1] >= s1.v2[1]){
       cout << "5 ";
       a = (s1.v1[0]- s1.v2[0]) * (s1.v1[1]- s1.v2[1]);
-    }else if(s1.v2[0] > s2.v1[0] && s2.v2[1] > s1.v1[1] && s2.v2[0] > s1.v2[0] && s2.v2[1] < s1.v2[1]){// s1 a esquerda de s2
+    }else if(s1.v2[0] > s2.v1[0] && s2.v2[1] > s1.v1[1] && s2.v2[0] > s1.v2[0] && s2.v2[1] < s1.v2[1]){
       cout << "1 ";
       a = (s1.v2[0] - s2.v1[0])*(s2.v2[1] - s1.v1[1]);
-    }else if(s1.v2[0] > s2.v1[0] && s1.v2[1] > s2.v1[1] && s2.v2[1] > s1.v2[1] && s2.v2[0] > s1.v2[0]){// s1 a direita de s2
+    }else if(s1.v2[0] > s2.v1[0] && s1.v2[1] > s2.v1[1] && s2.v2[1] > s1.v2[1] && s2.v2[0] > s1.v2[0]){
       cout << "2 ";
       a = (s1.v2[0] - s2.v1[0])*(s1.v2[1] - s2.v1[1]);
-    }else if(s1.v1[0] == s2.v1[0] && s2.v2[0] == s1.v2[0] && s2.v2[1] > s1.v2[1]){
+    }else if(s1.v1[0] <= s2.v1[0] && s2.v2[0] <= s1.v2[0] && s2.v2[1] > s1.v2[1]){
       cout << "3 ";
-      a = (s1.v2[0] - s2.v1[0])*(s1.v2[1] - s2.v1[1]);
-    }else if(s1.v1[1] == s2.v1[1] && s1.v2[1] == s2.v2[1] && s1.v1[0] < s2.v1[0]){
+      a = (s2.v2[0] - s2.v1[0])*(s1.v2[1] - s2.v1[1]);
+    }else if(s1.v1[1] >= s2.v1[1] && s1.v2[1] <= s2.v2[1] && s1.v1[0] < s2.v1[0]){
       cout << "4 ";
       a = (s1.v2[0] - s2.v1[0])*(s1.v2[1] - s1.v1[1]);
     }else{
